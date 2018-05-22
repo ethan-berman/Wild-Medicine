4d6ad09f6a4e0792e656e35db8c162dec6ce0235
Ethan Berman <ethanberman@ethans-mbp-4.milton.edu>
2018-05-22 11:05:39 -0400

    work on patient assessment

---
+++ Wild-Medicine/Ailment.swift
@@ -0,0 +1,21 @@
+//
+//  Ailment.swift
+//  Wild-Medicine
+//
+//  Created by Ethan Berman on 5/21/18.
+//  Copyright © 2018 Ethan Berman. All rights reserved.
+//
+
+import Foundation
+class Ailment{
+    var low_hr = Int()
+    var high_hr = Int()
+    var low_rr = Int()
+    var high_rr = Int()
+    struct signs{
+        
+    }
+    struct symptoms{
+        
+    }
+}
--- Wild-Medicine/Patient.swift
+++ Wild-Medicine/Patient.swift
@@ -48,4 +48,42 @@ class Patient{
         print("feelings prior to events: " + events)
         print("Spinal Injury? " + String(spinal))
     }
+    func redFlag()-> [String]{
+//        compare vital signs to the normal vital ranges to see if there are major red flags
+        var problems = [String]()
+        if(lor != "A+Ox4"){
+            problems.append("lor")
+        }
+        if(hr < 50 || hr > 100){
+            problems.append("hr")
+        }
+        if(rr < 12 || rr > 20){
+            problems.append("rr")
+        }
+        if(sctm != "pwd"){
+            problems.append("sctm")
+        }
+        if(syst < 120 && dias < 80){
+            problems.append("bp")
+        }
+        if(p != "PERRL"){
+            problems.append("p")
+        }
+        if(t > 100.4){
+            problems.append("t")
+        }
+        return problems
+    }
+    func assess() -> Ailment{
+        let problems = redFlag()
+        let cancer = Ailment()
+        if(problems.count == 0){
+//            if no vital issues, then turn to sample results or findings from physical exam
+        }else{
+//            sort through issues in vital signs and return potential ailments to furhter narrow later
+        }
+        
+        
+        return cancer
+    }
 }
